b0062f7946b8ebf563df6e0c2cd354bafa4996ea
Neolux Lee <neolux_lee@outlook.com>
2025-02-08 22:10:17 +0800

    run script

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-cd ~/app/go-proxypool
-./proxypool -c config/config.yaml
+if [ -e ~/proxypool ]; then
+	# Cloud version
+	echo "Using cloud verion de config"
+	cd ~/proxypool
+	./proxypool -c config/config.yaml
+else
+	# local version
+	echo "Using local verion de config"
+	cd ~/app/go-proxypool
+	./proxypool -c config/config.local.yaml
+fi
+
