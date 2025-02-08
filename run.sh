51bdc065082249325de53a46ab390240b42b3da3
Neolux Lee <neolux_lee@outlook.com>
2025-02-08 22:11:35 +0800

    run script

--- run.sh
+++ run.sh
@@ -3,12 +3,18 @@
 if [ -e ~/proxypool ]; then
 	# Cloud version
 	echo "Using cloud verion de config"
-	cd ~/proxypool
+	cd ~/proxypool -v
+	echo "Syncing from git repo"
+	git fetch
+	git pull
 	./proxypool -c config/config.yaml
 else
 	# local version
 	echo "Using local verion de config"
-	cd ~/app/go-proxypool
+	cd ~/app/go-proxypool -v
+	echo "Syncing from git repo"
+	git fetch
+	git pull
 	./proxypool -c config/config.local.yaml
 fi
 
